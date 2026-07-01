8df027d3227a21b6eea94693d53cda4bf4815093
Alejandro Camerlengo <ale_camer@hotmail.com>
2026-07-01 09:04:52 +0200

    feat: configure robust retry policies and daily schedule for main DAG

--- dags/dag_main.py
+++ dags/dag_main.py
@@ -24,8 +24,10 @@ default_args = {
     "depends_on_past": False,
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
+    "retries": 3,
+    "retry_exponential_backoff": True,
     "retry_delay": timedelta(minutes=2),
+    "max_retry_delay": timedelta(minutes=10),
 }
 
 
